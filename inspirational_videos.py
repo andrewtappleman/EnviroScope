d7383a38bffe598a4d46c9f0bdef0a8a3070c563
Andrew Appleman <andrewtappleman@gmail.com>
2024-09-26 17:54:16 -0400

    This was to clear out website screens.

--- inspirational_videos.py
+++
@@ -1,6 +0,0 @@
-from kivy.app import App
-from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.core.window import Window
-
-class InspirationalVideos(Screen):
-    pass
--- video1.py
+++
@@ -1,4 +0,0 @@
-from kivy.uix.screenmanager import ScreenManager, Screen
-
-class Video1(Screen):
-    pass
